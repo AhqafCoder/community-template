b49987cf0d8a81f1f3d099946dce7c35743c2a77
Badal saxena <saxenabadal420@gmail.com>
2025-02-22 08:18:57 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
-import Joinus from './components/Joinus'
+import Joinus from './components/Joinus' 
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,10 @@ function App() {
       <Navbar />
       <Hero />
       <Joinus />
-      <Footer />
+      
+      <Contact />
+      <Footer /> 
+   
     </>
   )
 }
